a1d2bd07d7c4d35950e17a53373628e23837ea35
Max G <maxg24@aol.com>
2014-05-21 16:34:55 -0400

    First draft

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function creates and caches the inversed matrix in variable ix
+## The second function will use the product of the first function to either computer solve or use cache inversed matrix
 
-## Write a short comment describing this function
+## if m is your matrix, for example, m=rbind(c(1, -1/8), c(-1/8, 1)), then
+## 1) call f <- makeCacheMatrix(m)
+## 2) then call cacheSolve(f)
+
+## the first run will force the function to compute the solve() 
+## the consequtive runs will use the cached data with message displayed "getting cached inversed matrix"
 
 makeCacheMatrix <- function(x = matrix()) {
-	## Test
+	## ix is the variable for reversed x variable if x is invertible matrix
+        ix <- NULL
+        set <- function(y) {
+                x <<- y
+                ix <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) ix <<- inverse
+        getinverse <- function() ix 
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
@@ -12,4 +28,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ix <- x$getinverse()
+        if(!is.null(ix)) {
+                message("getting cached inversed matrix")
+                return(ix)
+        }
+        data <- x$get()
+        ix <- solve(data, ...)
+        x$setinverse(ix)
+        ix
 }
